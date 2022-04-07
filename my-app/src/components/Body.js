c16de5062210b1dcc3f1d9a92bb178422476976e
shaunsmerling <shaunsmerling@gmail.com>
2022-04-07 15:22:26 -0700

    first commit

---
+++ my-app/src/components/Body.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Body() {
+  return (
+    <div className="Body">
+     
+    </div>
+  );
+}
+
+export default Body;
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import logo from "./EKLogo.png"
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <navbar>
+       <img src={logo} className="logo"></img>
+      </navbar>
+      <input 
+        type="text"
+
+
+     />
+    </div>
+  );
+}
+
+export default Navbar;
